9b16dddaa2c465a2627d9fdc196afd42691c0110
dcy <766320183@qq.com>
2016-07-09 18:32:50 +0800

    add mqtt javascript client

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -12,4 +12,49 @@ $(document).ready(function(){
 		}
 	)
 
+	METRICS = ["/subscriptions/total", "/clients/total", "/clients/active",
+		"/clients/inactive", "/memory/system", "/memory/total", '/cpuinfo/toal'
+	]
+
+	function sub_metrics(client, node) {
+		for (metric in METRICS) {
+			topic = '$SYS/' + node + METRICS[metric]
+			client.subscribe(topic)
+		}
+	}
+	client = new Paho.MQTT.Client("127.0.0.1", 8888, "fuck")
+	// set callback handlers
+	client.onConnectionLost = onConnectionLost;
+	client.onMessageArrived = onMessageArrived;
+
+	// connect the client
+	client.connect({onSuccess:onConnect});
+
+
+	// called when the client connects
+	function onConnect() {
+		// Once a connection has been made, make a subscription and send a message.
+		console.log("onConnect");
+
+		sub_metrics(client, 'VerneMQ@127.0.0.1')
+		//message = new Paho.MQTT.Message("Hello");
+		//message.destinationName = "/World";
+		//client.send(message);
+	}
+
+	// called when the client loses its connection
+	function onConnectionLost(responseObject) {
+		if (responseObject.errorCode !== 0) {
+			console.log("onConnectionLost:"+responseObject.errorMessage);
+		}
+	}
+
+	// called when a message arrives
+	function onMessageArrived(message) {
+		console.log("Message topic ", message.destinationName)
+		console.log("onMessageArrived:"+message.payloadString)
+	}
+
+	
+
 })
--- src/vmq_handler.erl
+++ src/vmq_handler.erl
@@ -7,7 +7,6 @@ init(_Transport, Req, []) ->
 	{ok, Req, undefined}.
 
 handle(Req, State) ->
-	lager:info("****** 8==D vmq_handler"),
 	Body = case cowboy_req:binding(what, Req) of
 			   {undefined, Req} ->
 				   jsx:encode(#{hello=>hello});
@@ -21,10 +20,8 @@ handle(Req, State) ->
 		Body, Req),
 	{ok, Req2, State}.
 
-
 terminate(_Reason, _Req, _State) ->
 	ok.
-
 
 
 
@@ -32,13 +29,22 @@ terminate(_Reason, _Req, _State) ->
 get_node_info(NodeName, IsRunning) ->
 	case IsRunning of
 		true ->
-			[{_, MqttIp, MqttPort, _, _, MaxConns}, {_, VmqIp, VmqPort, _, _, _}] =
-			rpc:call(NodeName, vmq_ranch_config, listeners, []),
-			#{name=>NodeName, is_running=>IsRunning,
-			  vmq_ip=>list_to_binary(VmqIp), vmq_port=>list_to_binary(VmqPort),
-			  mqtt_ip=>list_to_binary(MqttIp), mqtt_port=>list_to_binary(MqttPort),
-			  max_conns=>MaxConns};
+			Listeners = rpc:call(NodeName, vmq_ranch_config, listeners, []),
+			format_node(Listeners, #{name=>NodeName, is_running=>IsRunning});
 		false ->
 			#{name=>NodeName, is_running=>IsRunning, vmq_ip=>"", vmq_port=>"",
 			  mqtt_ip=>"", mqtt_port=>"", max_conns=>0}
 	end.
+
+format_node(Listeners, Node) ->
+	Fun = fun({Type, Ip, Port, _, _, MaxConns}, TempNode) ->
+				  IpKey = list_to_atom(lists:concat([Type, '_ip'])),
+				  PortKey = list_to_atom(lists:concat([Type, '_port'])),
+				  MaxConnsKey = list_to_atom(lists:concat([Type, '_max_conns'])),
+				  TempNode#{IpKey=>list_to_binary(Ip), PortKey=>list_to_binary(Port),
+							MaxConnsKey=>MaxConns}
+		  end,
+	lists:foldl(Fun, Node, Listeners).
+
+
+	
